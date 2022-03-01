683fe8e5467bfea70eedd924814b990d74bcb26b
Bogdan <76659274+Sav1oroff@users.noreply.github.com>
2022-03-01 22:52:20 +0300

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import './_vendor';
+import vars from './_vars';
+import './_functions';
+import './_components';
+import './components/_burger-menu';
+import './components/_typing-text';
+import './app';
+import './particles.min';
